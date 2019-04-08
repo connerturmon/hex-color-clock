d5378e7e21ebea229992800d7edbf44b9491a6c8
Conner <connerturmon@gmail.com>
2019-04-07 20:04:43 -0400

    Updates

---
+++ precache-manifest.4a07791c11e9e10b1ac503b398cf0aad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "78b476ea9593b081ad26",
+    "url": "/hex-color-clock/static/css/main.6112ad3a.chunk.css"
+  },
+  {
+    "revision": "78b476ea9593b081ad26",
+    "url": "/hex-color-clock/static/js/main.fc8d4499.chunk.js"
+  },
+  {
+    "revision": "43f4ea7a6416c5353263",
+    "url": "/hex-color-clock/static/js/runtime~main.9f2d8c22.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/hex-color-clock/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "fd1464a41a353cbd179a3694a0dca9a3",
+    "url": "/hex-color-clock/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/hex-color-clock/precache-manifest.4a07791c11e9e10b1ac503b398cf0aad.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/hex-color-clock/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
